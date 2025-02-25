c6d083f27c4948e3ab314e3528af7ea2de0542e1
aaguilarv <aaguilarv@loggro.com>
2025-02-24 20:34:31 -0500

    se organiza ingesta.py

--- src/bigdata/ingesta.py
+++ src/bigdata/ingesta.py
@@ -3,23 +3,22 @@ import json
 
 # Se consume con un json la librería requests
 
-def obtener_datos_api(url="", params={}):
-    url = "{}/{}/{}".format(url,params["coin"],params["method"])
+def obtener_datos_api(url="",params={}):
+    url = "{}/{}/{}/".format(url,params["coin"],params["method"])
     try:
-        response = request.get(url)
+        response = requests.get(url)
         response.raise_for_status()
         return response.json()
-    except  request.exceptions.RequestException as error:
+    except requests.exceptions.RequestException as error:
         print(error)
         return {}
     
-parametros = {"coin":"BTC","method":"ticket"}
+
+parametros = {"coin":"BTC","method":"ticker"}
 url = "https://www.mercadobitcoin.net/api"
-datos =  obtener_datos_api(url=url, params=parametros)
+datos = obtener_datos_api(url=url, params=parametros)
 
 if len(datos)>0:
-    print(json.dumps(datos, indent=4))
+    print(json.dumps(datos,indent=4))
 else:
-    print("No se obtuvo la consulta")
-    
-                            
+    print("no se obtubo la consulta")
